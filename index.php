0bc57a749149dfbf2f7bf34b3093b7da9937c94e
Bilal Abro <lala64467@gmail.com>
2022-01-04 20:41:59 +0500

    php function created

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
-	echo "Hello Github";
+	
+	$testarray = array(10 , 10 , 10 , 10 , 20);
+
+	function array_count($data){
+		$var1 = 0;
+		foreach ($data as $key => $value) {
+			$var1++;
+		}
+		return $var1;
+	}
+
+	echo array_count($testarray);
 
  ?>
